88999bc3b225ceaa031b1dcd90ccabdb1650ddbb
Ethan Gundry <gundryethan@gmail.com>
2017-06-22 14:07:19 -0700

    bugtest

--- main.py
+++ main.py
@@ -10,41 +10,41 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
         <form method="POST">
             <label>Rotate by </label>
             <input type="text" name="rot" value="0">
-            <textarea name="text"></textarea>
+            <textarea name="text">{0}</textarea>
             <input type="submit" value="Submit query"
     </body>
 </html>"""
 
 @app.route("/")
 def index():
-	return form
+	return form.format("")
 
 @app.route("/", methods=['POST'])
 def encrypt():
-	rot = request.form['rot']
+	rot = int(request.form['rot'])
 	text = request.form['text']
 
 	string = rotate_string(text, rot)
 
-	return string
+	return form.format(string)
 
 app.run()
